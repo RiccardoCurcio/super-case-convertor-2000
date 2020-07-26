8fc35fbaf931a3977bfa62899d2bf6c93221867d
curcioriccardo@gmail.com <curcioriccardo@gmail.com>
2020-07-26 10:11:30 +0200

    readme.md

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
+
 # Super-case-convertor-2000
-description
+
+### Quick start
+
+pip install
+
+```
+pip3 install super-case-converter-2000
+```
+
+use
+
+```
+from converter import case
+```
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="super-case-converter",
+    name="super-case-converter-2000",
     version="0.0.1",
     author="Riccardo Curcio",
     author_email="curcioriccardo@gmail.com",
@@ -13,6 +13,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/RiccardoCurcio/super-case-convertor-2000.git",
     packages=setuptools.find_packages(),
+    install_requires=['typing'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GPL v3 License",
